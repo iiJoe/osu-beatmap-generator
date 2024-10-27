9711a7a8910fef2b499ac3e599699eca73fedeae
Joe <jeremy.ongJW@gmail.com>
2024-10-28 03:56:51 +0800

    Develop (#5)
    * MLP changed to sequential with sigmoid
    
    * Implemented fast transformer
    
    * Removed unused imports
    
    * Switching to hugging tree's AST implementation
    
    * Retrieved last hidden state from ast (#4)

--- src/models/fine_tuned_model.py
+++ src/models/fine_tuned_model.py
@@ -1,14 +1,21 @@
-import torch, torch.nn as nn
+import torch.nn as nn
 import constants
 
 class FineTunedModel(nn.Module):
 
-    def __init__(self, model):
+    def __init__(self, model, transformer):
         super(FineTunedModel, self).__init__()
         self.model = model
-        self.mlp = nn.Linear(constants.label_dim, 1)
+        self.transformer = transformer
+        self.mlp = nn.Sequential(
+            nn.Linear(constants.label_dim, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
 
     def forward(self, x):
-        model_output = self.model(x)
-        mlp_output = self.mlp(model_output)
-        return mlp_output.squeeze(-1)
+        model_output = self.model(x).last_hidden_state
+        transformer_output = self.transformer(model_output)
+        mlp_output = self.mlp(transformer_output)
+        return mlp_output
--- src/train.py
+++ src/train.py
@@ -4,7 +4,8 @@ import torch, torch.nn as nn
 from torch.utils.data import DataLoader
 
 import constants
-from models.ast_model import ASTModel
+from transformers import ASTModel
+from models.fast_transformers import fast_transformer
 from models.fine_tuned_model import FineTunedModel
 
 def labels_from_csv(csv_file):
@@ -19,9 +20,9 @@ def train():
     dataset = AudioDataset(file_paths, labels)
     data_loader = DataLoader(dataset, batch_size=len(file_paths), shuffle=True)
 
-    ast_mdl = ASTModel(label_dim=constants.label_dim, audioset_pretrain=True)
-    fine_tuned_model = FineTunedModel(ast_mdl)
-    criterion = nn.BCEWithLogitsLoss() # Applies sigmoid internally
+    ast_mdl = ASTModel.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593", num_labels=constants.label_dim)
+    fine_tuned_model = FineTunedModel(ast_mdl, fast_transformer)
+    criterion = nn.BCELoss()
     optimizer = torch.optim.Adam(fine_tuned_model.parameters(), lr=0.0001)
 
     num_epochs = 10
@@ -40,6 +41,6 @@ def train():
 
         print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(data_loader):.4f}')
 
-        torch.save(fine_tuned_model.state_dict(),     "ast/pretrained_models/trained_model.pth")
+        torch.save(fine_tuned_model.state_dict(), "../pretrained_models/trained_model.pth")
 
 train()
