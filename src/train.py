87288471bf984ab0d757c391695e87eadc7bbac9
Joe <jeremy.ongJW@gmail.com>
2024-12-13 10:48:42 +0800

    Refined training and eval (#6) (#7)
    * Split Dataset into train-test, fixed AST output shape
    
    * Finalized first implementation of fast transformers
    
    * fine tuned hyper params

--- src/train.py
+++ src/train.py
@@ -1,46 +1,53 @@
-import numpy as np
 from dataloader import AudioDataset
 import torch, torch.nn as nn
 from torch.utils.data import DataLoader
 
 import constants
-from transformers import ASTModel
-from models.fast_transformers import fast_transformer
-from models.fine_tuned_model import FineTunedModel
-
-def labels_from_csv(csv_file):
-    data = np.genfromtxt(csv_file, delimiter=",", dtype=str, encoding='utf-8')
-    file_paths = data[:, 0]
-    labels = data[:, 1:].astype(float)
-
-    return file_paths, labels
+from utils import labels_from_csv, get_model
 
 def train():
-    file_paths, labels = labels_from_csv(constants.labels_file)
-    dataset = AudioDataset(file_paths, labels)
-    data_loader = DataLoader(dataset, batch_size=len(file_paths), shuffle=True)
+    file_paths, file_labels = labels_from_csv(constants.training_labels_file)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    batch_size = constants.batch_size
 
-    ast_mdl = ASTModel.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593", num_labels=constants.label_dim)
-    fine_tuned_model = FineTunedModel(ast_mdl, fast_transformer)
-    criterion = nn.BCELoss()
-    optimizer = torch.optim.Adam(fine_tuned_model.parameters(), lr=0.0001)
+    print("=== Training Start ===")
 
-    num_epochs = 10
-    fine_tuned_model.train()
-    for epoch in range(num_epochs):
+    print(f"Using weight of {constants.label_weight}")
+    # Split data set to train batch by batch
+    for i in range(0, len(file_paths), batch_size):
+        print(f"Batch {i // batch_size + 1} Start")
+        end_idx = min(i+batch_size, len(file_paths))
+        print(f"Processing records from index {i} to {end_idx}")
 
-        running_loss = 0.0
-        for batch_spectrograms, batch_labels in data_loader:
-            optimizer.zero_grad()
-            outputs = fine_tuned_model(batch_spectrograms)
-            loss = criterion(outputs, batch_labels.float())
+        dataset = AudioDataset(file_paths[i:end_idx], file_labels[i:end_idx])
+        data_loader = DataLoader(dataset, batch_size=end_idx - i, shuffle=True)
 
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
+        num_epochs = 20
+        model = get_model()
+        model.to(device)
+        model.train()
 
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(data_loader):.4f}')
+        for epoch in range(num_epochs):
+            running_loss = 0.0
+            for data, labels in data_loader:
+                data, labels = data.to(device, non_blocking=True), labels.to(device, non_blocking=True)
+                weights = torch.where(labels == 1, constants.label_weight, 1.0)
+                criterion = nn.BCELoss(weight=weights)
+                optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
-        torch.save(fine_tuned_model.state_dict(), "../pretrained_models/trained_model.pth")
+                optimizer.zero_grad()
+                outputs = model(data)
+                loss = criterion(outputs, labels.float())
+
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+
+            print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(data_loader):.4f}')
+
+            torch.save(model.state_dict(), constants.trained_model_path)
+        print(f"Batch {i // batch_size + 1} End")
+
+    print("=== Training End ===")
 
 train()
