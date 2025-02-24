d7ef637b3d1a5599a5464dea416109fe5598d2c7
Joe <jeremy.ongJW@gmail.com>
2025-02-24 09:56:36 +0800

    Feat/encoder decoder (#8) (#9)
    * Updated preprocessor script for new model
    
    * New model and positional encoding
    
    * Refined training, added eval
    
    * Added inference & normalization
    
    * Added validation check during training

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,8 +1,8 @@
-from pydub import AudioSegment
 import os
+import json
 import constants
 import glob
-import numpy as np
+from utils import splice_audio
 
 # Split the osu files into the following sections
 # ['General', 'Editor', 'Metadata', 'Difficulty', 'Events', 'TimingPoints', 'Colours', 'HitObjects']
@@ -52,46 +52,22 @@ def hit_objects_dict(list):
   hit_objects = []
   for item in list:
     values = item.split(",")
-    hit_object = dict(zip(constants.osu_keys, values))
+    hit_object = dict(zip(constants.hit_object_keys, values))
     hit_objects.append(hit_object)
 
   return hit_objects
 
-# 10.24s intervals
-def splice_audio(file_path, beatmap_id, interval_ms=constants.seq_length * 10):
-    _, file_name = os.path.split(file_path)
-    prefix, file_extension = os.path.splitext(file_name)
-    file_extension = file_extension[1:] # Remove leading dot
-    new_directory = constants.splice_directory
-
-    if not os.path.exists(new_directory):
-      os.mkdir(new_directory)
-
-    audio = AudioSegment.from_file(file_path)
-    audio_duration = len(audio)
-    audio_splices = []
-
-    for i in range(0, audio_duration, interval_ms):
-
-        chunk = audio[i:i + interval_ms]
-        chunk_name = os.path.join(new_directory, f"{beatmap_id}-{prefix}_{i // interval_ms}.{file_extension}")
-        chunk.export(chunk_name, format=file_extension)
-        audio_splices.append(chunk_name)
-
-    return audio_splices
-
 def preprocess():
     osu_folders = [name for name in os.listdir(constants.beatmaps_directory) if os.path.isdir(os.path.join(constants.beatmaps_directory, name))]
 
-    # Clearing the labels file to ensure no duplicates of data
-    with open(constants.training_labels_file, 'w') as file:
-        pass
-    with open(constants.test_labels_file, 'w') as file:
-        pass
+    if not osu_folders:
+        print(f"No beatmap folders found in {constants.beatmaps_directory}.")
+        return
 
     beatmaps_count = 0
     splices_count = 0
     total_rows = []
+
     for beatmap_set_id in osu_folders:
 
         new_directory = constants.beatmaps_directory + '/' + beatmap_set_id
@@ -114,26 +90,51 @@ def preprocess():
           spliced_audio_paths = splice_audio(audio_directory, beatmap_id)
           splices_count += len(spliced_audio_paths)
 
-          # Beat timings
-          hit_timings_data = [int(hit_object["time"]) for hit_object in hit_objects_data]
-          hit_timings = np.full((len(spliced_audio_paths), constants.seq_length), "0", dtype=str)
+          # Only considering hit circle notes, may consider spinners and sliders in the future
           hit_timing_delimiter = constants.seq_length * 10
-          for timing in hit_timings_data:
-            hit_timings[timing // hit_timing_delimiter][(timing % hit_timing_delimiter) // 10] = "1"
-          prepended_paths = np.array(spliced_audio_paths).reshape(-1, 1)
-          rows = np.concatenate((prepended_paths, hit_timings), axis=1)
-          total_rows.append(rows)
+          note_index = 0
+          type_bitmask = (1 << 0) | (1 << 2) | (1 << 4) | (1 << 5) | (1 << 6) # Only considering hit circle and combo bits
+          for i in range(0, len(spliced_audio_paths)):
+              if note_index == len(hit_objects_data):
+                  break
 
-    total_rows = np.vstack(total_rows)
-    shape = total_rows.shape[0]
-    train_rows, test_rows = np.split(total_rows[np.random.permutation(shape)], [int(0.8 * shape)])
+              attributes = []
+              hit_object = hit_objects_data[note_index]
 
-    with open(constants.training_labels_file, "ab") as file:
-        np.savetxt(file, train_rows, delimiter=",", fmt="%s")
-    with open(constants.test_labels_file, "ab") as file:
-        np.savetxt(file, test_rows, delimiter=",", fmt="%s")
+              while (int(hit_object["time"]) < (i + 1) * hit_timing_delimiter):
+                  tpe = (int(hit_object["type"]) & type_bitmask)
+                  if not tpe:
+                      tpe = 1
 
-    print(f"Processed {beatmaps_count} beatmaps with {splices_count} splices of audio")
-    print(f"Split dataset into {train_rows.shape[0]} splices for training and {test_rows.shape[0]} splices for test")
+                  attributes.append({
+                      "x" : int(hit_object["x"]),
+                      "y" : int(hit_object["y"]),
+                      "time": int(hit_object["time"]) % hit_timing_delimiter // 10,
+                      "type": tpe,
+                      "hitSound": int(hit_object["hitSound"])
+                  })
+
+                  note_index += 1
+                  if note_index == len(hit_objects_data):
+                      break
+
+                  hit_object = hit_objects_data[note_index]
+
+              data = {
+                  constants.json_file_path_key: spliced_audio_paths[i],
+                  constants.json_attributes_key: attributes
+              }
+              total_rows.append(data)
+
+    if total_rows:
+        split_delimiter = int(len(total_rows) * 0.8)
+        train_rows, test_rows = [total_rows[:split_delimiter], total_rows[split_delimiter:]]
+        with open(constants.training_labels_file, "w") as file:
+            json.dump(train_rows, file, indent=4)
+        with open(constants.test_labels_file, "w") as file:
+            json.dump(test_rows, file, indent=4)
+
+        print(f"Processed {beatmaps_count} beatmaps with {splices_count} splices of audio")
+        print(f"Split dataset into {len(train_rows)} splices for training and {len(test_rows)} splices for test")
 
 preprocess()
--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,15 @@
-import numpy as np
-
-from models.fast_transformers import fast_transformer
-from models.fine_tuned_model import FineTunedModel
-from models.pos_embed import PositionalEncoding
+from models.osu_model import OsuModel
+from pydub import AudioSegment
 import os
 import torch
+import torch.nn.functional as F
+import numpy as np
+import librosa
+import json
 import constants
 
+
+# Deprecated, used for old model
 def labels_from_csv(csv_file):
     data = np.genfromtxt(csv_file, delimiter=",", dtype=str, encoding='utf-8')
     file_paths = data[:, 0]
@@ -14,13 +17,135 @@ def labels_from_csv(csv_file):
 
     return file_paths, labels
 
-def get_model():
-    pos_embed = PositionalEncoding(constants.seq_length, 128)
-    fine_tuned_model = FineTunedModel(pos_embed, fast_transformer)
-    print(f"Retrieving model...")
-    if os.path.exists(constants.trained_model_path):
-        print(f"Trained model exists. Loading saved state")
-        checkpoint = torch.load(constants.trained_model_path)
-        fine_tuned_model.load_state_dict(checkpoint)
 
-    return fine_tuned_model
+def labels_from_json(json_file):
+    with open(json_file) as json_data:
+        max_len = 0
+        file_paths = []
+        attributes_list = []
+        empty_token = {
+            constants.exists_key: 2,
+            **{k: 0 for k in constants.predictions_keys if k != constants.exists_key}
+        }
+
+        data = json.load(json_data)
+        for splice in data:
+            attributes = splice[constants.json_attributes_key]
+            file_paths.append(splice[constants.json_file_path_key])
+            for i in range(len(attributes)):
+                attributes[i][constants.exists_key] = 1 if i < len(attributes) - 1 else 0
+            max_len = max(max_len, len(attributes))
+            attributes_list.append(attributes)
+
+        padded_attributes_list = []
+        for attributes in attributes_list:
+            # Exists set to 2 to denote padded
+            padded_attributes = attributes + [empty_token] * (max_len - len(attributes))
+            padded_attributes_list.append(padded_attributes)
+
+        final_attributes_list = np.array([
+            [[
+                attr[key] for key in constants.predictions_keys
+            ]
+             for attr in attrs]
+            for attrs in padded_attributes_list])
+        return file_paths, final_attributes_list
+
+
+def get_model():
+    print("Retrieving model...")
+    model = OsuModel(
+        nhead=8,
+        num_encoder_layers=6,
+        num_decoder_layers=6,
+        dim_feedforward=1024,
+        dropout=0.1
+    )
+    if os.path.exists(constants.trained_model_path):
+        print("Trained model exists. Loading saved state")
+        checkpoint = torch.load(constants.trained_model_path)
+        model.load_state_dict(checkpoint)
+
+    return model
+
+def get_model_infer():
+    print("Retrieving model...")
+    model = OsuModel(
+        nhead=8,
+        num_encoder_layers=6,
+        num_decoder_layers=6,
+        dim_feedforward=1024,
+        dropout=0.1
+    )
+    if os.path.exists(constants.best_model_path):
+        print("Loading best model")
+        checkpoint = torch.load(constants.best_model_path)
+        model.load_state_dict(checkpoint)
+
+    return model
+
+
+def causal_mask(seq_len):
+    mask = torch.triu(torch.ones(seq_len, seq_len, dtype=torch.bool), diagonal=1)
+    return mask
+
+
+def audio_to_spectrogram_tensor(audio_file):
+    y, sr = librosa.load(audio_file, sr=None, duration=10.24)
+    n_fft = 256
+    hop_length = 441
+    S = librosa.stft(y, n_fft=n_fft, hop_length=hop_length)
+    S_db = librosa.amplitude_to_db(np.abs(S))
+
+    spectrogram = S_db[:128, :1024].T
+
+    spectrogram_tensor = torch.tensor(spectrogram, dtype=torch.float32)
+    padded_tensor = pad_spectrogram_tensor(spectrogram_tensor)
+    normalized_tensor = normalize_tensor(padded_tensor)
+
+    return normalized_tensor
+
+
+def pad_spectrogram_tensor(tensor, target_shape=(1024, 128)):
+    current_shape = tensor.shape
+
+    if current_shape == target_shape:
+        return tensor
+
+    padding_height = target_shape[0] - current_shape[0]
+
+    if padding_height > 0:
+        tensor = F.pad(tensor, (0, 0, 0, padding_height), mode='constant', value=0)
+
+    return tensor
+
+
+# Z-score Normalization
+def normalize_tensor(tensor):
+    mean = tensor.mean()
+    std = tensor.std()
+    return (tensor - mean) / (std * 2)
+
+
+# 10.24s intervals
+def splice_audio(file_path, beatmap_id, interval_ms=constants.seq_length * 10):
+    _, file_name = os.path.split(file_path)
+    prefix, file_extension = os.path.splitext(file_name)
+    file_extension = file_extension[1:]  # Remove leading dot
+    new_directory = constants.splice_directory
+
+    if not os.path.exists(new_directory):
+        os.mkdir(new_directory)
+
+    audio = AudioSegment.from_file(file_path)
+    audio_duration = len(audio)
+    audio_splices = []
+
+    for i in range(0, audio_duration, interval_ms):
+
+        chunk = audio[i:i + interval_ms]
+        chunk_name = os.path.join(new_directory, f"{beatmap_id}-{prefix}_{i // interval_ms}.{file_extension}")
+        chunk.export(chunk_name, format=file_extension)
+        audio_splices.append(chunk_name)
+
+    return audio_splices
